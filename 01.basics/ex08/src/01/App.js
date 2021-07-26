39bcfb41d3c8c14e056dec94f118664312cc2f1f
heeyomi <yhg9508@gmail.com>
2021-07-26 11:28:39 +0900

    JSX Tutorials #2

---
+++ 01.basics/ex08/src/01/App.js
@@ -0,0 +1,32 @@
+import React, { createElement } from 'react';
+
+export default function() {
+    return (
+        <div>
+            <h2>App01</h2>
+            <p>JSX Tutorials - 특징1: HTML과의 차이점</p>
+            {/*
+                1. 속성은 Camel Cased
+            */}
+            <input type='text' maxLength='10' />
+
+            {/*
+                2. Element는 꼭 닫혀야 한다.
+                오류) <br>, <hr>, <input type='text'> <img src=''>
+            */}
+            <br/>
+            <hr/>
+            <img width='120px' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2560px-React-icon.svg.png'/>
+            
+            {/*
+                3. 속성 이름은 DOM API 기반이다.
+                <div id='box' class='box'><div>
+                document.getElementById('box').className = 'box'
+            */}
+            <div id='box' className='box'>
+                box 입니다.
+            </div>
+        </div>    
+    );
+
+}
