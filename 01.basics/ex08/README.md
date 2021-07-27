98e03572697df2edebb47988dfb42823db5123b7
heeyomi <yhg9508@gmail.com>
2021-07-27 09:15:13 +0900

    JSX Tutorials #6

--- 01.basics/ex08/README.md
+++ 01.basics/ex08/README.md
@@ -4,7 +4,7 @@
 ### 02. 특징2: Single Root Node
 ### 03. JSX로 함수 콤포넌트 작성하기
 ### 04. Pure React(React API)로 함수 콤포넌트 작성
-### 05. 클래스 콤포넌트 작성하기
+### 05. 클래스 컴포넌트 작성하기
 ### 06. 특징3: JSX 표현식 표기법 { _expression_ } 문제점: if-statement
 ### 07. 특징3: 공백(Blank Space)
 ### 08. Dynamic HTML Rendering
---
+++ 01.basics/ex08/src/06/Clock01.js
@@ -0,0 +1,27 @@
+import React, { Component, Fragment } from 'react';
+
+export default function() {
+    const date = new Date();
+
+    let hour = date.getHours();
+    let minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+
+    let session = 'AM';
+    if (hour > 12 ){
+        hour -= 12;
+        session = 'PM';
+    }
+    hour = ('0' + hour).slice(-2);
+
+    return (
+        <div>
+            { hour }
+            :
+            { minutes }
+            :
+            { seconds }
+            { session }     
+        </div>
+    )
+}
