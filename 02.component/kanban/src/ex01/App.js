56c6ed0a5f5d62a16651d0b0da41805fd0e48461
heeyomi <yhg9508@gmail.com>
2021-07-28 10:50:49 +0900

    Kanban #2

---
+++ 02.component/kanban/src/ex01/App.js
@@ -0,0 +1,8 @@
+import React, { createElement, Component } from 'react';
+import KanbanBoard from './KanbanBoard';
+
+export default function App(){
+    return (
+        <KanbanBoard />
+    );
+}
