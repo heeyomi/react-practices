3f74260cdc92bc7bfadc801d849b1f96ca641a6e
heeyomi <yhg9508@gmail.com>
2021-08-02 12:14:38 +0900

    inline styling - kanban

--- 02.component/kanban/src/ex02/CardList.js
+++ 02.component/kanban/src/ex02/CardList.js
@@ -3,11 +3,10 @@ import Card from './Card';
 import styles from '../assets/css/CardList.css';
 
 export default function CardList( { title, cards }){
-    console.log(title, cards);
     return(
         <div className={styles.CardList}>
             <h1>{title}</h1>
-            { cards.map(card => <Card key={card.no} title={card.title} description={card.description} tasks={card.tasks}/>) }
+            { cards.map(card => <Card key={card.no} title={card.title} description={card.description} status={card.status} tasks={card.tasks}/>) }
         </div>
     );
 }
