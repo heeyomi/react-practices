ef6544526da0597101ef04debdcb44b47d83da78
heeyomi <yhg9508@gmail.com>
2021-08-05 11:10:45 +0900

    webpack5 지원

--- 01.basics/ex07/src/App.js
+++ 01.basics/ex07/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import logo from './assets/images/logo.png';
-import styles from './assets/css/App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <header className={styles['App-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
-        <p>
-          React Week 과제 입니다.
-        </p>
+    <div className={'App'}>
+      <header className={'App-header'}>
+        <img src={logo} className={'App-logo'} alt="logo" />
         <h1>Hello World</h1>
       </header>
     </div>
