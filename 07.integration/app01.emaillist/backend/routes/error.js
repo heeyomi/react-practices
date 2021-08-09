45d28c530eefb6864929f3be24f5532a212aff5e
heeyomi <yhg9508@gmail.com>
2021-08-09 16:16:21 +0900

    app01.emaillist #finish

--- 07.integration/app01.emaillist/backend/routes/error.js
+++ 07.integration/app01.emaillist/backend/routes/error.js
@@ -1,8 +1,8 @@
 const logger = require('../logging');
 
 module.exports = {
-    error404: function(req, res){
-        if(req.accepts('html')){
+    error404: function(req, res) {
+        if(req.accepts('html')) {
             res.status(404).render('error/404')
             return;
         }
@@ -11,19 +11,19 @@ module.exports = {
         res.status(404).send({
             result: 'fail',
             data: null,
-            message: 'Unknown Request'
-        });
+            message: 'unknown request'
+        });        
     },
-    error500: function(err, req, res, next){
+    error500: function(err, req, res, next) {
         logger.error(err.stack);
-        
-        if(req.accepts('html')){
+
+        if(req.accepts('html')) {
             res.status(500).render('error/500', {
-                error : err.stack
+                   error: err.stack 
             });
             return;
         }
-        
+
         /* response json */
         res.status(500).send({
             result: 'fail',
