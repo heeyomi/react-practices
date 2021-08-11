f104f290562b036372358b2a91beb5d8138736ae
heeyomi <yhg9508@gmail.com>
2021-08-11 10:31:53 +0900

    property valaidation

---
+++ 02.component/kanban/src/ex04/TaskList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Task from './Task';
+import styles from '../assets/css/TaskList.css';
+import PropTypes from 'prop-types';
+
+export default function TaskList({tasks}) {
+    return (
+        <div className={styles.TaskList}>
+            <ul>
+                { tasks.map(task => <Task key={ task.no } name={ task.name } />) }
+            </ul>
+        </div>
+    );
+}
+
+TaskList.propType = {
+    tasks : PropTypes.arrayOf(PropTypes.object).isRequired
+}
